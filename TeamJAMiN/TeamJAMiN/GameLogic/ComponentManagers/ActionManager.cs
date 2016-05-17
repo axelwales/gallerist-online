384efba985aa700bd1d59df0187f3ee254fa2993
Axel Wales <axelwales@gmail.com>
2016-05-17 17:45:48 -0500

    Added transition validation functions (to be called by a view or other object outside of the action invoker).

--- TeamJAMiN/TeamJAMiN/GameLogic/ComponentManagers/ActionManager.cs
+++ TeamJAMiN/TeamJAMiN/GameLogic/ComponentManagers/ActionManager.cs
@@ -8,11 +8,11 @@ namespace TeamJAMiN.Controllers.GameLogicHelpers
 {
     public enum PendingPosition
     {
-        first,last
+        first, last
     }
     public static class ActionManager
     {
-        
+
         public static GameAction GetTicketAction(VisitorTicketType type)
         {
             GameAction action = new GameAction();
@@ -38,6 +38,40 @@ namespace TeamJAMiN.Controllers.GameLogicHelpers
         {
             var invoker = new ActionContextInvoker(game);
             return invoker.IsValidGameState(action);
+        }
+
+        public static bool IsValidAction(GameActionState state, string actionLocation, Game game, GameAction parent = null)
+        {
+            var action = new GameAction { State = state, Location = actionLocation, Parent = parent };
+            return action.IsValidAction(game);
+        }
+
+        public static bool IsValidTransition(this GameAction action, Game game)
+        {
+            var invoker = new ActionContextInvoker(game);
+            return invoker.IsValidTransition(action);
+        }
+
+        public static bool IsValidTransition(GameActionState state, string actionLocation, Game game, GameAction parent = null)
+        {
+            var action = new GameAction { State = state, Location = actionLocation, Parent = parent };
+            return action.IsValidTransition(game);
+        }
+
+        public static bool IsValidTicketBonus(GameActionState state, Game game, VisitorTicketType type, GameAction parent = null)
+        {
+            if (parent == null)
+            {
+                parent = game.CurrentTurn.CurrentAction.Parent;
+            }
+            var actionLocation = type.ToString();
+            return IsValidTransition(state, actionLocation, game, parent);
+        }
+
+        public static bool IsValidTicketBonus(Game game, VisitorTicketType type)
+        {
+            var state = game.CurrentTurn.CurrentAction.State;
+            return IsValidTicketBonus(state, game, type);
         }
     }
 }
