979ce0ebee73a5c8f3fa30c6344c9a6c4cef786a
Axel Wales <axelwales@gmail.com>
2016-05-22 09:34:33 -0500

    Added money and fame booleans for influence track actions

--- TeamJAMiN/TeamJAMiN/Models/ComponentViewModels/InfluenceTrackViewModel.cs
+++ TeamJAMiN/TeamJAMiN/Models/ComponentViewModels/InfluenceTrackViewModel.cs
@@ -12,23 +12,26 @@ namespace TeamJAMiN.Models.ComponentViewModels
         public InfluenceTrackViewModel(Game game)
         {
             Spaces = new List<InfluenceSpaceViewModel>();
+            var currentPlayerInfluence = game.CurrentPlayer.Influence;
+
             for(int i = 0; i<36; i++)
             {
                 if (game.Players.Any(p => p.Influence == i))
                 {
                     var playerColors = game.Players.Where(p => p.Influence == i).Select(p => p.Color).ToList();
-                    Spaces.Add(new InfluenceSpaceViewModel(i, playerColors));
+                    Spaces.Add(new InfluenceSpaceViewModel(i, currentPlayerInfluence, playerColors ));
                 }
                 else
-                    Spaces.Add(new InfluenceSpaceViewModel(i));
+                    Spaces.Add(new InfluenceSpaceViewModel(i, currentPlayerInfluence));
             }
         }
+
         public List<InfluenceSpaceViewModel> Spaces { get; private set; }
     }
 
     public class InfluenceSpaceViewModel
     {
-        public InfluenceSpaceViewModel(int index, List<PlayerColor> colors) : this(index)
+        public InfluenceSpaceViewModel(int index, int  currentPlayerInfluence, List<PlayerColor> colors) : this(index, currentPlayerInfluence)
         {
             var ulHtml = "<ul class=\"player-influence-markers\">";
             foreach (PlayerColor color in colors)
@@ -39,20 +42,26 @@ namespace TeamJAMiN.Models.ComponentViewModels
             ulHtml += "</ul>";
             InfluenceMarkerHtml = ulHtml;
         }
-        public InfluenceSpaceViewModel(int index)
+        public InfluenceSpaceViewModel(int index, int currentPlayerInfluence)
         {
             Index = index;
             DisplayIndex = index.ToString();
             MoneyHtml = StarHtml = KickedOutCssClass = InfluenceMarkerHtml = "";
+            UseInfluenceAsMoney = UseInfluenceAsFame = false;
+
             if (GameInfluenceTrack.InfluenceToMoney.Contains(index))
             {
                 var money = Array.IndexOf(GameInfluenceTrack.InfluenceToMoney, index);
                 MoneyHtml = @"<div class=""influence-money-icon money-" + money + @"""></div>";
+                if(index < currentPlayerInfluence)
+                    UseInfluenceAsMoney = true;
             }
             if (index % 5 == 0 && index != 35)
             {
                 StarHtml = @"<div class=""star-white influence-star-icon""></div>";
                 KickedOutCssClass = "has-star ";
+                if (index < currentPlayerInfluence)
+                    UseInfluenceAsFame = true;
             }
             if (index % 5 == 0 && index != 0)
             {
@@ -70,5 +79,8 @@ namespace TeamJAMiN.Models.ComponentViewModels
         public string StarHtml { get; private set; }
         public string KickedOutCssClass { get; private set; }
         public string InfluenceMarkerHtml { get; private set; }
+
+        public bool UseInfluenceAsMoney { get; private set; }
+        public bool UseInfluenceAsFame { get; private set; }
     }
 }
